e7a143062f89d1e7ac9cdb7d85f9e3e6fbe760f5
zealtron <jqw3ha@virginia.edu>
2014-11-28 13:23:04 -0500

    filled in rest of lights

--- Ray/rayPointLight.todo.cpp
+++ Ray/rayPointLight.todo.cpp
@@ -24,4 +24,14 @@ Point3D RayPointLight::transparency(RayIntersectionInfo& iInfo,RayShape* shape,P
 // OpenGL stuff //
 //////////////////
 void RayPointLight::drawOpenGL(int index){
+	GLfloat light_position[] = {location[0], location[1], location[2], 1};	
+	glLightfv(GL_LIGHT0+index, GL_POSITION, light_position);
+	GLfloat diffuse[] = {color.p[0], color.p[1], color.p[2]};
+	glLightfv(GL_LIGHT0+index, GL_DIFFUSE, diffuse);
+	glLightf(GL_LIGHT0+index, GL_CONSTANT_ATTENUATION, constAtten);
+	glLightf(GL_LIGHT0+index, GL_LINEAR_ATTENUATION, linearAtten);
+	glLightf(GL_LIGHT0+index, GL_QUADRATIC_ATTENUATION, quadAtten);	
+	glEnable(GL_LIGHT0+index);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
 }
--- Ray/raySpotLight.todo.cpp
+++ Ray/raySpotLight.todo.cpp
@@ -25,6 +25,14 @@ Point3D RaySpotLight::transparency(RayIntersectionInfo& iInfo,RayShape* shape,Po
 //////////////////
 void RaySpotLight::drawOpenGL(int index){
 	GLfloat spot_direction[] = {direction.negate().p[0], direction.negate().p[1], direction.negate().p[2], 0.0};
+	glLightf(GL_LIGHT0+index, GL_SPOT_CUTOFF, cutOffAngle);
 	glLightfv(GL_LIGHT0+index, GL_SPOT_DIRECTION, spot_direction);
+	GLfloat diffuse[] = {color.p[0], color.p[1], color.p[2]};
+	glLightfv(GL_LIGHT0+index, GL_DIFFUSE, diffuse);
+	glLightf(GL_LIGHT0+index, GL_CONSTANT_ATTENUATION, constAtten);
+	glLightf(GL_LIGHT0+index, GL_LINEAR_ATTENUATION, linearAtten);
+	glLightf(GL_LIGHT0+index, GL_QUADRATIC_ATTENUATION, quadAtten);	
 	glEnable(GL_LIGHT0+index);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
 }
