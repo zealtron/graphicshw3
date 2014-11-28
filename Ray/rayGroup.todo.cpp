3537a1621ff58359561d80f4afeabd3523650e01
zealtron <jqw3ha@virginia.edu>
2014-11-28 14:15:57 -0500

    Transformation works

--- Ray/rayGroup.todo.cpp
+++ Ray/rayGroup.todo.cpp
@@ -50,10 +50,20 @@ int RayGroup::getOpenGLCallList(void){
 }
 
 int RayGroup::drawOpenGL(int materialIndex){	
-	glMatrixMode(GL_MODELVIEW);
+	glMatrixMode(GL_MODELVIEW);	
+	Matrix4D m = getMatrix();
+	GLdouble matrix[16] = {
+		m(0,0), m(0,1), m(0,2), m(0,3),
+		m(1,0), m(1,1), m(1,2), m(1,3),
+		m(2,0), m(2,1), m(2,2), m(2,3),
+		m(3,0), m(3,1), m(3,2), m(3,3)
+	};
+	glPushMatrix();
+	glMultMatrixd(matrix);
 	for (int i = 0; i < sNum; i++) {
 		int distance = shapes[i]->drawOpenGL(materialIndex);
 	}
+	glPopMatrix();
 	return -1;
 }
 
