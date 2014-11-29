9e54813ce3938a4621de9c3a0b0590663aea69fc
zealtron <jqw3ha@virginia.edu>
2014-11-28 21:40:56 -0500

    mouse drag rotation done

--- Ray/rayCamera.todo.cpp
+++ Ray/rayCamera.todo.cpp
@@ -11,12 +11,12 @@ void RayCamera::drawOpenGL(void){
 	gluLookAt(position.p[0], position.p[1], position.p[2], direction.p[0], direction.p[1], direction.p[2], up.p[0], up.p[1], up.p[2]);
 }
 void RayCamera::rotateUp(Point3D center,float angle){
-	Point3D k = (center-position).unit();
-	direction = direction*cos(angle*PI/180) + (k.crossProduct(direction))*sin(angle*PI/180) + k*(k.dot(direction))*(1-cos(angle*PI/180));	
+	Point3D k = up;
+	position = position*cos(angle) + (k.crossProduct(position))*sin(angle) + k*(k.dot(position))*(1-cos(angle));	
 }
-void RayCamera::rotateRight(Point3D center,float angle){	
-	Point3D k = (center-position).unit();	
-	up = up*cos(angle*PI/180) + (k.crossProduct(up))*sin(angle*PI/180) + k*(k.dot(up))*(1-cos(angle*PI/180));	
+void RayCamera::rotateRight(Point3D center,float angle){
+	Point3D k = direction.crossProduct(up);
+	position = position*cos(angle) + (k.crossProduct(position))*sin(angle) + k*(k.dot(position))*(1-cos(angle));	
 }
 void RayCamera::moveForward(float dist){
 }
